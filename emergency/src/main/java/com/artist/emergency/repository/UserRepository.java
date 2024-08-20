d8f5cc9fc3caae7cfbd4220c5904da02b1f02d2e
adduup <adwait.uprety21@gmail.com>
2024-08-19 21:59:18 -0500

    commit 9

--- emergency/src/main/java/com/artist/emergency/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.artist.emergency.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.artist.emergency.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
