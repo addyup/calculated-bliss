85c59557567829889850715eb020d95213ef7356
adduup <adwait.uprety21@gmail.com>
2024-08-24 18:58:01 -0500

    added cross origin hooks.

--- e-vented-viewer/src/app/event.service.ts
+++ e-vented-viewer/src/app/event.service.ts
@@ -15,6 +15,10 @@ export class EventService {
   getDreamvilleEvents(): Observable<string> {
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': 'true',
+                'Access-Control-Allow-Headers': 'Content-Type',
+                'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE'
     });
 
     return this.http.get(`${this.baseUrl}/dreamville`, { headers: headers, responseType: 'text' });
