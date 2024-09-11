21f8d244fd41d6c8bfe35b84ad12121a4ec3a1b0
adduup <adwait.uprety21@gmail.com>
2024-09-10 22:12:50 -0500

    added only website details

--- css-stylist/src/app/app.routes.ts
+++ css-stylist/src/app/app.routes.ts
@@ -1,6 +1,4 @@
 import { Routes } from '@angular/router';
-import { LogoComponent } from './logo/logo.component';
-import { SocialLinksComponent } from './social-links/social-links.component';
 import { SignupComponent } from './signup/signup.component';
 
 export const routes: Routes = [
