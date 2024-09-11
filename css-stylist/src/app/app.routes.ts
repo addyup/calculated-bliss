fb6943c7f97bc58ae6df93bd05006b53f9b586b5
adduup <adwait.uprety21@gmail.com>
2024-09-11 17:50:14 -0500

    check it outt 2

--- css-stylist/src/app/app.routes.ts
+++ css-stylist/src/app/app.routes.ts
@@ -2,6 +2,6 @@ import { Routes } from '@angular/router';
 import { SignupComponent } from './signup/signup.component';
 
 export const routes: Routes = [
-  { path: 'signup', component: SignupComponent },
+  { path: 'checkitout', component: SignupComponent },
   { path: '', redirectTo: '/checkitout', pathMatch: 'full' }
 ];
