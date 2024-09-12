ab6deef4b8bc2198a26aa2ba7b815eb3859d4e6c
adduup <adwait.uprety21@gmail.com>
2024-09-12 11:18:49 -0500

    added business

--- css-stylist/src/app/app.routes.ts
+++ css-stylist/src/app/app.routes.ts
@@ -2,6 +2,6 @@ import { Routes } from '@angular/router';
 import { SignupComponent } from './signup/signup.component';
 
 export const routes: Routes = [
-  { path: 'checkitoutt', component: SignupComponent },
-  { path: '', redirectTo: '/checkitoutt', pathMatch: 'full' }
+  { path: 'checkitout', component: SignupComponent },
+  { path: '', redirectTo: '/checkitout', pathMatch: 'full' }
 ];
