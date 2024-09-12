05d08481862359eff93168a71263f04fabd2d256
adduup <adwait.uprety21@gmail.com>
2024-09-12 17:42:15 -0500

    changed path

--- css-stylist/src/app/app.routes.ts
+++ css-stylist/src/app/app.routes.ts
@@ -2,6 +2,6 @@ import { Routes } from '@angular/router';
 import { SignupComponent } from './signup/signup.component';
 
 export const routes: Routes = [
-  { path: 'checkitout', component: SignupComponent },
-  { path: '', redirectTo: '/checkitout', pathMatch: 'full' }
+  { path: 'leflair', component: SignupComponent },
+  { path: '', redirectTo: '/leflair', pathMatch: 'full' }
 ];
