6c56c869ab2f7bad92151f71746f143d7061415e
adduup <adwait.uprety21@gmail.com>
2024-09-12 19:10:08 -0500

    changed controller filepath

--- demo/src/main/java/com/elephantScrape/demo/controller/BillboardController.java
+++ demo/src/main/java/com/elephantScrape/demo/controller/BillboardController.java
@@ -1,4 +1,4 @@
-package com.elephantScrape.demo;
+package com.elephantScrape.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
