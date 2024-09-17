99d670fb47674bdde26d87c0b37c9da467e901f9
adduup <adwait.uprety21@gmail.com>
2024-09-17 10:25:31 -0500

    oppen

--- css-stylist/src/app/signup/leflair.component.ts
+++ css-stylist/src/app/signup/leflair.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MandeerEventListComponent } from "../mandeer-event-list/mandeer-event-list.component";
-import { TextGeneratorComponent } from "../text-generator/text-generator.component";
 
 
 @Component({
   selector: 'app-signup',
   templateUrl: './leflair.component.html',
   styleUrls: ['./leflair.component.css'],
-  imports: [FormsModule, MandeerEventListComponent, TextGeneratorComponent],
+  imports: [FormsModule, MandeerEventListComponent,],
   standalone: true
 })
 export class SignupComponent {
