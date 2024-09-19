9f096002fea293a045d797d600f03577bba63061
adduup <adwait.uprety21@gmail.com>
2024-09-19 13:31:17 -0500

    2

--- websc/src/main/java/com/mandeer/websc/BuildCbeController.java
+++ websc/src/main/java/com/mandeer/websc/BuildCbeController.java
@@ -1,6 +1,7 @@
 package com.mandeer.websc;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.mandeer.websc.model.BuildDetails;
 
 @RestController
+@CrossOrigin
 public class BuildCbeController {
 
     private final EmailBuildCbeService emailService;
